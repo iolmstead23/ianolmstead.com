90f2f5618fe7530c577f5a402ffafff23ec911db
iolmstead23 <72961956+iolmstead23@users.noreply.github.com>
2023-09-01 09:07:43 -0500

    modified:   app/page.js

--- app/page.js
+++ app/page.js
@@ -3,15 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <Fade duration='10000'>
-        <p>GeeksforGeeks - Text</p>
-      </Fade>
-      <Flip>
-        <p>Second Text</p>
-      </Flip>
-      <Bounce>
-        <p>Third Text</p>
-      </Bounce>
+      Hello World!
     </div>
   )
 }
