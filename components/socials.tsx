2827b38f9957d2411d532db44b2c740ca1dad210
iolmstead23 <72961956+iolmstead23@users.noreply.github.com>
2024-03-04 22:22:38 -0600

    Totally Rocker Dude

--- components/socials.tsx
+++ components/socials.tsx
@@ -5,9 +5,15 @@ export default function Socials() {
           <div className="mx-auto max-w-2xl space-y-16 divide-y divide-gray-100 lg:mx-0 lg:max-w-none">
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
               <div>
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900">Get in touch</h2>
+                <h2 className="text-3xl font-bold tracking-tight text-gray-900">About Me</h2>
                 <p className="mt-4 leading-7 text-gray-600">
-                  I use different social media platforms for different needs. Here is a list of my socials.
+                
+Hey there, rockers!
+I'm Ian Olmstead, and buckle up because you're about to dive headfirst into my coding odyssey. It all started when I was just a teen, cranking out RPG and zombie video games like there was no tomorrow. Back then, it was just a cool way to kill time, but little did I know it was laying the foundation for something epic.
+Fast forward to today, and after a bit of soul-searching (and maybe a touch of enlightenment), I decided it was time to turn my passion into a full-blown career. Cue the rockstar moment: snagging my Applied Data Science certificate from MIT in 2020. And guess what? I'm not just letting it collect dust—I'm putting that bad boy to work every single day.
+But the journey's not over yet—I'm still shredding through my Bachelor's degree in Computer Science like a true rock god. And when I'm not hitting the books, you can bet I'm out there, pounding the pavement at Hackathons across the Midwest. Because let's face it, nothing gets my adrenaline pumping like a bit of coding chaos.
+So here's the deal: I'm not just here to code—I'm here to rock your world. Whether it's understanding your personal vibe or fine-tuning your business needs, I'm all about making magic happen. So, what do you say? Ready to crank up the volume and join me on this wild ride?
+Let's do this!
                 </p>
               </div>
               <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
